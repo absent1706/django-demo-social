ca0c481a8694813c3a23fca6590a1902d528ea81
Alexander Litvinenko <litvinenko1706@gmail.com>
2016-01-22 18:26:52 +0200

    + passowrd reset by email

--- account/urls.py
+++ account/urls.py
@@ -25,4 +25,23 @@ urlpatterns = [
         'django.contrib.auth.views.password_change_done',
         {'template_name': 'account/password_change_done.html'},
         name='password_change_done'),
+
+    # restore password urls
+    url(r'^password-reset/$',
+        'django.contrib.auth.views.password_reset',
+        {'template_name': 'account/password_reset_form.html',
+        'email_template_name':'account/password_reset_email.html'},
+        name='password_reset'),
+    url(r'^password-reset/done/$',
+        'django.contrib.auth.views.password_reset_done',
+        {'template_name': 'account/password_reset_done.html'},
+        name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        {'template_name': 'account/password_reset_confirm.html'},
+        name='password_reset_confirm'),
+    url(r'^password-reset/complete/$',
+        'django.contrib.auth.views.password_reset_complete',
+        {'template_name': 'account/password_reset_complete.html'},
+        name='password_reset_complete'),
 ]
