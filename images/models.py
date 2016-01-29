5700a5a5b2afe5491a4c41bf43eb1d239b0da610
Alexander Litvinenko <litvinenko1706@gmail.com>
2016-01-29 16:36:47 +0200

    + detail view for image

--- images/models.py
+++ images/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-
+from django.core.urlresolvers import reverse
 from django.utils.text import slugify
 
 
@@ -25,3 +25,6 @@ class Image(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(Image, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
